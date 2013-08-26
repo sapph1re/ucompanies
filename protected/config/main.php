2318c12724563ee413709b9dceddd21e8a5e7987
Roman Vinogradov <sapphire@rollertown.ru>
2013-08-26 20:25:38 +0400

    Authentication using our database table instead of the built-in demo:demo

--- protected/config/main.php
+++ protected/config/main.php
@@ -47,19 +47,15 @@ return array(
 			),
 		),
 		*/
+
 		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
-		// uncomment the following to use a MySQL database
-		/*
-		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'connectionString' => 'mysql:host=localhost;dbname=users_and_companies',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => '',
+			'password' => 'bugaga',
 			'charset' => 'utf8',
 		),
-		*/
+
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
 			'errorAction'=>'site/error',
